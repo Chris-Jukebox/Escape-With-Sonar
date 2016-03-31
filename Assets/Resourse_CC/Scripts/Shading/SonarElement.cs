f2dc95018cb775a4bd7ff704ce84a24bcfba8939
Cecilia <xic.cecilia@gmail.com>
2016-03-30 22:29:38 -0400

    update sonar wave elements

--- Assets/Resourse_CC/Scripts/Shading/SonarElement.cs
+++ Assets/Resourse_CC/Scripts/Shading/SonarElement.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class SonarElement : MonoBehaviour {
 
+    private static float SPEED = 0.12f;
+    private static int MASK = (1 << 8);
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+        // move forward
+        transform.position = transform.position + transform.forward * SPEED * Time.deltaTime;
 	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        // collide with obstacles
+        if (col.gameObject.layer == 8)
+        {
+            // get texture coord
+            RaycastHit hit;
+            if (!Physics.Raycast(transform.position, transform.forward, out hit))
+                return;
+
+            // texture
+        }
+    }
 }
